adc0ef4c8836f973b71343cbe2f53d256f9d4114
nuggy875 <jinugpaeng@gmail.com>
2022-02-24 18:35:42 +0900

    model structure

---
+++ models/backbone.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn.functional as F
+import torchvision
+from torch import nn
+from torchvision.models._utils import IntermediateLayerGetter
+
+from .positional_encoding import PositionalEncoding
+
+
+class FrozenBatchNorm2d(torch.nn.Module):
+    """
+    BatchNorm2d where the batch statistics and the affine parameters are fixed.
+
+    Copy-paste from torchvision.misc.ops with added eps before rqsrt,
+    without which any other models than torchvision.models.resnet[18,34,50,101]
+    produce nans.
+    """
+
+    def __init__(self, n):
+        super(FrozenBatchNorm2d, self).__init__()
+        self.register_buffer("weight", torch.ones(n))
+        self.register_buffer("bias", torch.zeros(n))
+        self.register_buffer("running_mean", torch.zeros(n))
+        self.register_buffer("running_var", torch.ones(n))
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        num_batches_tracked_key = prefix + 'num_batches_tracked'
+        if num_batches_tracked_key in state_dict:
+            del state_dict[num_batches_tracked_key]
+
+        super(FrozenBatchNorm2d, self)._load_from_state_dict(
+            state_dict, prefix, local_metadata, strict,
+            missing_keys, unexpected_keys, error_msgs)
+
+    def forward(self, x):
+        # move reshapes to the beginning
+        # to make it fuser-friendly
+        w = self.weight.reshape(1, -1, 1, 1)
+        b = self.bias.reshape(1, -1, 1, 1)
+        rv = self.running_var.reshape(1, -1, 1, 1)
+        rm = self.running_mean.reshape(1, -1, 1, 1)
+        eps = 1e-5
+        scale = w * (rv + eps).rsqrt()
+        bias = b - rm * scale
+        return x * scale + bias
+
+
+class Backbone(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        model = getattr(torchvision.models, 'resnet50')(replace_stride_with_dilation=[False,False,True], pretrained=True, norm_layer=FrozenBatchNorm2d)
+        self.backbone = IntermediateLayerGetter(model, return_layers={'layer4': "0"})
+        self.num_channels = 2048
+
+        self.positional_encoding = PositionalEncoding(self.num_channels//2 , normalize=True)  # Not Learnable Version
+
+    def forward(self):
+        # feat = self.backbone(image)
+        # feat = self.positional_encoding(feat)
+        return 0
--- models/detr.py
+++ models/detr.py
@@ -2,11 +2,13 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
+from .backbone import Backbone
+
 
 class DETR(nn.Module):
     def __init__(self, num_classes=81, num_queries=100):
         super().__init__()
-        # self.backbone = ResNet50
+        # self.backbone = Backbone()
         # self.transformer = Transformer
         self.num_queries = num_queries
         self.num_classes = num_classes
