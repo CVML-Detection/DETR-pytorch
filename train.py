28b23f89ecd1942f576381917553284fd0cc77a2
csm-kr <csm8167@naver.com>
2022-04-05 18:40:07 +0900

    add train function

---
+++ train.py
@@ -0,0 +1,63 @@
+import time
+import os
+import torch
+import torch.distributed as dist
+from config import device
+
+
+def train(epoch, vis, train_loader, model, criterion, optimizer, scheduler, opts):
+    print('Training of epoch [{}]'.format(epoch))
+    tic = time.time()
+    model.train()
+
+    for idx, data in enumerate(train_loader):
+
+        images = data[0]
+        targets = data[1]
+        images = images.to(device)
+        outputs = model(images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        loss = criterion(outputs, targets)
+
+        # update
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        toc = time.time()
+
+        for param_group in optimizer.param_groups:
+            lr = param_group['lr']
+
+        # for each steps
+        if idx % opts.vis_step == 0 or idx == len(train_loader) - 1:
+            print('Epoch: [{0}]\t'
+                  'Step: [{1}/{2}]\t'
+                  'Loss: {loss:.4f}\t'
+                  'Learning rate: {lr:.7f} s \t'
+                  'Time : {time:.4f}\t'
+                  .format(epoch, idx, len(train_loader),
+                          loss=loss,
+                          lr=lr,
+                          time=toc - tic))
+
+            if vis is not None:
+                # loss plot
+                vis.line(X=torch.ones((1, 1)).cpu() * idx + epoch * train_loader.__len__(),  # step
+                         Y=torch.Tensor([loss]).unsqueeze(0).cpu(),
+                         win='train_loss',
+                         update='append',
+                         opts=dict(xlabel='step',
+                                   ylabel='Loss',
+                                   title='training loss',
+                                   legend=['Total Loss']))
+
+    if not os.path.exists(opts.save_path):
+        os.mkdir(opts.save_path)
+
+    checkpoint = {'epoch': epoch,
+                  'model_state_dict': model.state_dict(),
+                  'optimizer_state_dict': optimizer.state_dict(),
+                  'scheduler_state_dict': scheduler.state_dict()}
+
+    torch.save(checkpoint, os.path.join(opts.save_path, opts.save_file_name + '.{}.pth.tar'.format(epoch)))
