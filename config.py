c91e11b69ff301c1b47d9661aaeed4e1d0570126
csm-kr <csm8167@naver.com>
2022-04-07 15:47:49 +0900

    convert vis_step to 10

--- config.py
+++ config.py
@@ -14,20 +14,20 @@ def parse(args):
     parser.add_argument('--lr', type=float, default=1e-4)               # initial lr (1e-5 after 200epoch)
     parser.add_argument('--lr_backbone', type=float, default=1e-5)      # backbone lr
     parser.add_argument('--weight_decay', type=float, default=1e-5)
-    parser.add_argument('--batch_size', type=int, default=64)           #
+    parser.add_argument('--batch_size', type=int, default=2)            #
 
     parser.add_argument('--layer_encoder', type=int, default=6)
     parser.add_argument('--layer_decoder', type=int, default=6)
 
-    parser.add_argument('--vis_step', type=int, default=100)
+    parser.add_argument('--vis_step', type=int, default=10)
     # parser.add_argument('--burn_in', type=int, default=4000)  # 64000 / b_s | b_s == 16 -> 4000 | b_s == 64 -> 1000
 
     # parser.add_argument('--num_workers', type=int, default=4)
     # parser.add_argument('--resize', type=int, help='320, 416, 608', default=416)
     parser.add_argument('--save_path', type=str, default='./saves')
-    parser.add_argument('--save_file_name', type=str, default='detr_coco')  # FIXME
+    parser.add_argument('--save_file_name', type=str, default='detr_coco_exp1')  # FIXME
     parser.add_argument('--conf_thres', type=float, default=0.05)
-    parser.add_argument('--start_epoch', type=int, default=4)
+    parser.add_argument('--start_epoch', type=int, default=0)
 
     # FIXME choose your dataset root
     #parser.add_argument('--data_root', type=str, default='/home/cvmlserver5/Sungmin/data/coco')
